f14c8ddada16bc541246c8061d26ffcc1bbb9a0b
Lucas Viana <oviana@outlook.com>
2022-08-04 19:20:06 -0300

    Setup push script

--- push.sh
+++ push.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 npm run build
+
+if [ -f ".env" ]; then
+  source .env
+fi
+
+clasp settings rootDir .build
+clasp settings scriptId "$SCRIPT_ID"
+cp appsscript.json .claspignore .clasp.json .build
 clasp push

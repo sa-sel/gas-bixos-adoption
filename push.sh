c86ae650c43c421110ff9cdd84402c376f50dfb9
Lucas Viana <oviana@outlook.com>
2022-08-04 20:40:27 -0300

    Update push script to work with CI/CD

--- push.sh
+++ push.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
-npm run build
+
+mkdir -p .build
 
 if [ -f ".env" ]; then
-  source .env
+  source ".env"
 fi
 
-clasp settings rootDir .build
-clasp settings scriptId "$SCRIPT_ID"
-cp appsscript.json .claspignore .clasp.json .build
+if [ ! -f ".clasp.json" ]; then
+  clasp clone "$SCRIPT_ID" --rootDir ".build" >> /dev/null
+else
+  clasp settings "rootDir" ".build" >> /dev/null
+  clasp settings "scriptId" "$SCRIPT_ID" >> /dev/null
+fi
+
+npm run build
+
+cp "appsscript.json" ".claspignore" ".clasp.json" ".build"
 clasp push
